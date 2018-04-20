f07a4d591983817fff9770699ba57108d4c4c7e3
Thorbjørn Christensen <cvcv90@gmail.com>
2018-04-20 09:51:22 +0200

    adds step update count_each_in_range

--- lib/enum_extra.ex
+++ lib/enum_extra.ex
@@ -7,11 +7,26 @@ defmodule EnumExtra do
   end
 
   def count_each_in_range(enumerable, min, max, range_size) do
+    empty_result =
+      step(min, max, range_size)
+      |> Enum.map(fn k -> {k, 0} end)
+      |> Enum.into(%{})
+
     enumerable
-    |> Enum.map(&(&1 - rem(&1, range_size)))
+    |> Enum.map(&(rem(min, range_size) + &1 - rem(&1, range_size)))
     |> count_each()
     |> Enum.filter(fn {k, v} -> k >= min and k <= max end)
+    |> Enum.into(%{})
+    |> Map.merge(empty_result, fn _k, val1, _val2 -> val1 end)
     |> Enum.sort_by(fn {k, _} -> k end)
+    |> Enum.map(fn {k, v} -> {{k, k + range_size}, v} end)
+  end
+
+  def step(min, max, step_size) do
+    n = Integer.floor_div(max - min, step_size)
+
+    Range.new(0, n)
+    |> Enum.map(&(&1 * step_size + min))
   end
 
   def average(enumerable) do
