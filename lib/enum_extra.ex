d68c60dfc5f26dcf98d5c9a3397339492eb49b63
Thorbjørn Christensen <cvcv90@gmail.com>
2018-04-20 09:22:14 +0200

    add count_each_in_range

--- lib/enum_extra.ex
+++ lib/enum_extra.ex
@@ -6,6 +6,14 @@ defmodule EnumExtra do
     Enum.reduce(enumerable, %{}, fn k, acc -> Map.update(acc, k, 1, &(&1 + 1)) end)
   end
 
+  def count_each_in_range(enumerable, min, max, range_size) do
+    enumerable
+    |> Enum.map(&(&1 - rem(&1, range_size)))
+    |> count_each()
+    |> Enum.filter(fn {k, v} -> k >= min and k <= max end)
+    |> Enum.sort_by(fn {k, _} -> k end)
+  end
+
   def average(enumerable) do
     Decimal.div(Decimal.new(Enum.sum(enumerable)), Decimal.new(Enum.count(enumerable)))
   end
